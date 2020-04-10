c998f8f07410fc83f399f517c22b29b82eedeed8
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-10 08:08:47 -0700

    oop1 complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+# Vehicle
+
+
+class Vehicle():
+    def __init__(self, name):
+        self.name = name
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
